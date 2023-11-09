fa80b48b3bf79052d55fc6bf70ba5f38afb7e262
Asif-GD <asif.coach@gmail.com>
2023-11-09 14:42:13 +0530

    explored math functions in Python3

---
+++ 03_python_basics_1/022_math_functions.py
@@ -0,0 +1,13 @@
+# in-built math functions
+
+# round (rounding floating point numbers)
+print(round(3.44267))
+print(round(3.7866))
+
+# absolute (returns the absolute value i.e. no negative value)
+print(abs(-25))
+
+# can abs take a floating point number? Yes
+print(abs(-32.5))
+
+# there are a lot more in-built math functions available in Python3. Google them.
