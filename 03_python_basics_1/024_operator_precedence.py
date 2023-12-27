a9f50084e5e2f27e84ebc34d19bb466f0644a45b
Asif-GD <asif.coach@gmail.com>
2023-12-27 12:33:40 +0530

    performed the exercise

--- 03_python_basics_1/024_operator_precedence.py
+++ 03_python_basics_1/024_operator_precedence.py
@@ -6,3 +6,15 @@ PEMDAS - Parenthesis, Exponent, Multiplication / Division, Addition / Subtractio
 + -
 """
 print((10 - 4) + 2 ** 2)
+
+# exercise
+
+print((5 + 4) * 10 / 2)  # 45.0
+
+print(((5 + 4) * 10) / 2)  # 45.0
+
+print((5 + 4) * (10 / 2))  # 45.0
+
+print(5 + (4 * 10) / 2)  # 25.0
+
+print(5 + 4 * 10 // 2)  # 25
