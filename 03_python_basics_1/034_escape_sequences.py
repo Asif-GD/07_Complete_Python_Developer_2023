548f3adfaf7c50f1c603b4d62710ea4d8064178f
Asif-GD <asif.coach@gmail.com>
2023-12-28 15:40:13 +0530

    checking out escape sequences

---
+++ 03_python_basics_1/034_escape_sequences.py
@@ -0,0 +1,8 @@
+"""
+Escape Sequences
+"""
+
+string_1 = 'Hi there! How\'s your day been so far?'  # we could also use a ' within " "
+string_2 = '\t It\'s been alright; \n and how\'s your day been so far?'  # \t - a tab & \n - a new line
+
+print(string_1, "\n", string_2)
