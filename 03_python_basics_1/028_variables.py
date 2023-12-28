ef49f9cd0edd80fa59492274da1a3e9791ebaa57
Asif-GD <asif.coach@gmail.com>
2023-12-28 10:16:16 +0530

    playing with variables

---
+++ 03_python_basics_1/028_variables.py
@@ -0,0 +1,23 @@
+"""
+variable - best practices
+
+1. snake_cases
+2. must start with a lower case or underscores but NOT numbers
+3. composed on letters, numbers, and/or underscores
+4. DO NOT overwrite keywords
+5. they are case-sensitive
+"""
+
+# a variable starting with an _ is reserved as the best practise for creating private variables in classes
+_user_iq = 170
+print(f"_user_iq = {_user_iq}")
+
+# a variable starting with __ (double underscores) is reserved the best practise creating dunder variables.
+__user = "Asif"
+print(f"__user = {__user}")
+
+# a shorthand form
+variable_1, variable_2, variable_3 = 1, 2, 3
+print(f"variable_1 = {variable_1}")
+print(f"variable_2 = {variable_2}")
+print(f"variable_3 = {variable_3}")
