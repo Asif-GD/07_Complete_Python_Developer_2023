bf44c08b315f6f4c034792123905550fbd45037f
Asif-GD <asif.coach@gmail.com>
2023-12-28 11:17:33 +0530

    notes on statements vs. expressions

--- 03_python_basics_1/028_variables.py
+++ 03_python_basics_1/028_variables.py
@@ -21,3 +21,12 @@ variable_1, variable_2, variable_3 = 1, 2, 3
 print(f"variable_1 = {variable_1}")
 print(f"variable_2 = {variable_2}")
 print(f"variable_3 = {variable_3}")
+
+"""
+expression vs. statement
+expression - any calculation that evaluates to a value
+statement - a line of code
+"""
+# this line below is a statement.
+variable_4 = variable_1 + variable_2 + variable_3  #  the code after the assignment operator = is an expression.
+print(f"variable_4 = {variable_4}")
