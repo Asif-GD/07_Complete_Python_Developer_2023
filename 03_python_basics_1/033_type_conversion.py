941a8b0ff679c28a6c0f9bd536787c40b487d053
Asif-GD <asif.coach@gmail.com>
2023-12-28 15:30:53 +0530

    trying out type conversions

---
+++ 03_python_basics_1/033_type_conversion.py
@@ -0,0 +1,7 @@
+"""
+type conversion or type casting -> converting a value of one data-type to another
+"""
+variable_1: [int] = 100
+variable_2 = str(variable_1)
+print(f"data-type of variable_1: {type(variable_1)}")
+print(f"data-type of variable_2: {type(variable_2)}")
