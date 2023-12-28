81f04d0f1a8b8e653e134b91e63aa498fe5cb639
Asif-GD <asif.coach@gmail.com>
2023-12-28 15:17:54 +0530

    playing with strings

---
+++ 03_python_basics_1/031_strings.py
@@ -0,0 +1,17 @@
+"""
+Strings
+"""
+
+string_1 = 'this is a string;'  # ' '
+string_2 = "this is a string too;"  # " "
+string_3 = """Multi-
+Line 
+string"""
+
+print(f"string_1: {string_1}")
+print(f"string_2: {string_2}")
+print(f"string_3: {string_3}")
+
+# string concatenation -> fancy way of saying "adding strings" together
+concatenated_string = string_1 + " & " + string_2
+print(f"concatenated_string: {concatenated_string}")
