46216f82a898524a48abf347eb681c277c22793b
Asif-GD <asif.coach@gmail.com>
2023-12-30 13:41:45 +0530

    making a practise of specifying type when declaring and creating new variables.

--- 03_python_basics_1/038_string_methods.py
+++ 03_python_basics_1/038_string_methods.py
@@ -4,13 +4,13 @@ playing around with string methods.
 - No matter what methods we use to act on the strings, the original string remains unchanged.
 """
 
-string_1 = "Asif Iqbal"
+string_1: [str] = "Asif Iqbal"
 
 # for example
 print(f"although, string_1.upper() is {string_1.upper()}")
 print(f"string_1 is still {string_1}; because strings are immutable.")
 
-string_2 = string_1.upper()
+string_2: [str] = string_1.upper()
 print(f"string_2 - {string_2}")
 
 # does this work? It does.
