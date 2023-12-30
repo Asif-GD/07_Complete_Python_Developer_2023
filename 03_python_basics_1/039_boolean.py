2534cf3dbf3a83dfcb53b7cb35cf3ff80dbc2e7c
Asif-GD <asif.coach@gmail.com>
2023-12-30 13:51:45 +0530

    playing with boolean datatype

---
+++ 03_python_basics_1/039_boolean.py
@@ -0,0 +1,15 @@
+"""
+exploring boolean data-type
+"""
+
+is_true: [bool] = True
+is_false: [bool] = False
+
+# bool conversion
+print(f"bool(1) = {bool(1)}")
+print(f"bool(0) = {bool(0)}")
+
+# what if
+print(f"bool(-1) = {bool(-1)}")
+print(f"bool('something') = {bool('something')}")
+# in conclusion, all values are True, and 0 is False.
